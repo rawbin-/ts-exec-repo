d10c3ee45a4eb119ee397c135af68e47e9c97efa
rawbin <fcrshjx44@gmail.com>
2025-05-03 12:34:29 +0800

    feat: 第二种方式， 使用commonjs兼容的方式处理

--- src/with-ts.ts
+++ src/with-ts.ts
@@ -1,5 +1,5 @@
-import grpc from '@grpc/grpc-js'
-import protoLoader from '@grpc/proto-loader'
+import * as grpc from '@grpc/grpc-js'
+import * as protoLoader from '@grpc/proto-loader'
 
 console.log(grpc,protoLoader)
 const packageDefinition = protoLoader.loadSync('./hello.proto', {
